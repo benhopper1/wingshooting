5199627668a15e27e46f3603b4f184d9bc3c59a6
benhopper1 <hopperdevelopment@gmail.com>
2014-12-17 08:36:35 -0600

    second

--- express/controllers/wingcontroller.js
+++ express/controllers/wingcontroller.js
@@ -20,18 +20,47 @@ module.exports.controller = function(app){
 					}});
 	});
 
+
+
+
 	app.get('/slider', function(req, res){
-		//console.log("/angtest");
-		console.log('Matt Controller routing view: matt to matt/matt.jade');
-		res.render('test/test1.php',{
+		console.log('/slider GET');
+		res.render('wing/slider.jade',
+			{
 				URL:configData.domain.address + ":" + configData.domain.port,
 				webSocketClient:configData.webSocketClient,
 				defaultUserImageUrl:configData.defaultUserImageUrl,
 				defaultMemberImageUrl:configData.defaultMemberImageUrl,
 				data:
 					{
-					}});
+					},
+				sliderSize:
+					{
+						width:600
+					},
+				sliderDivs:
+					[
+						'<div><a u=image href="#"><img src="/public/images/ads/main_000.png"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/images/ads/main_002.png"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/01.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/images/ads/main_002.png"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/02.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/03.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/04.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/05.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/images/ads/main_000.png"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/06.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/07.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/08.jpg"></img></a></div>',
+						'<div><a u=image href="#"><img src="/public/js/slider/img/landscape/10.jpg"></img></a></div>'
+					]
+			}
+		);
 	});
+
+
+
+
 
 	app.get('/slider2', function(req, res){
 		//console.log("/angtest");
@@ -45,6 +74,10 @@ module.exports.controller = function(app){
 					{
 					}});
 	});
+
+
+
+
 
 //   /public/js/slider/
 //http://wingshootingdestinations.com/upload/Ads/topad_90.gif
